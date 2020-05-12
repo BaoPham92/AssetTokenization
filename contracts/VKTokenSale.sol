06b6f39cb0d2ca38f35c5b28736747eb23034146
BaoPham92 <baopham92@yahoo.com>
2020-05-11 21:06:33 -0400

    Crowdsale smart contract, deployed, & transferred tokens to contract

---
+++ contracts/VKTokenSale.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.6.0;
+
+import "./crowdSale.sol";
+
+
+contract VKTokenSale is Crowdsale {
+    constructor(uint256 rate, address payable wallet, IERC20 token)
+        public
+        Crowdsale(rate, wallet, token) {
+
+        }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,12 @@
 const VKToken = artifacts.require("VKToken.sol");
+const VKTokenSale = artifacts.require("VKTokenSale.sol");
 
-module.exports = async (deployer) => await deployer.deploy(VKToken, 1000000000);
+module.exports = async (deployer) => {
+    let addr = await web3.eth.getAccounts();
+    let VKInstance = await VKToken.deployed();
+    console.log(await deployer)
+
+    await deployer.deploy(VKToken, 1000000000);
+    await deployer.deploy(VKTokenSale, 1, addr[0], VKToken.address);
+    await VKInstance.transfer(VKTokenSale.address, 1000000000);
+};
